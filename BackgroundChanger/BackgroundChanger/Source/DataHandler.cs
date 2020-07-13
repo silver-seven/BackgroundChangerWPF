3e073fd59c5192042c00d3e704b6ddfe08eb281c
joshqhub <32751655+joshqhub@users.noreply.github.com>
2020-07-13 12:39:00 -0400

    b

---
+++ BackgroundChanger/BackgroundChanger/Source/DataHandler.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace BackgroundChanger.Source
+{
+    class DataHandler
+    {
+        private IList<string> pathList = new List<string>();
+        private IList<string> favList = new List<string>();
+        private IList<string> excludeList = new List<string>();
+        private string fileName;
+        private XMLHandler xml = new XMLHandler();
+        private string[] xmlRootNodes;
+        private string[] xmlRootNodes2; /*Timer, BufferSize, WallpaperStyle, FavFreq*/
+        private int[] appParams; /*Timer, BufferSize, WallpaperStyle, FavFreq*/
+
+        public DataHandler()
+        {
+            fileName = "user.xml";
+            xmlRootNodes = new string[] { "Paths", "Favorites", "Excludes" };
+            xmlRootNodes2 = new string[] { "Timer", "BufferSize", "WallpaperStyle", "favFreq" };
+           
+            //Load/Create xml file.
+            if (File.Exists(fileName))
+            {
+                xml.Load();
+                for (int i = 0; i < xmlRootNodes2.Count(); i++)
+                {
+                    appParams[i] = int.Parse(xml.GetXMLNode("//" + xmlRootNodes2[i]));
+                }
+                xml.GetXMLNodes("//Paths", ref pathList);
+                ///////////////////////////////TODO: NEED TO LOAD TO LIST; FAV AND EXCLUDE
+            }
+            else
+            {
+                appParams = new int[] { 600, 100, 0, 10 };
+                xml.Create(fileName);
+                foreach(string str in xmlRootNodes)
+                {
+                    xml.AddToRoot(str);
+                }
+                for(int i=0; i < xmlRootNodes2.Count(); i++)
+                {
+                    xml.AddToRoot(xmlRootNodes2[i], appParams[i].ToString());
+                }
+            }
+        }
+
+        public void SaveData()
+        {
+            xml.Save();
+        }
+
+        //add
+        public void AddPath(string path)
+        {
+            pathList.Add(path);
+        }
+        public void AddFav(string fav)
+        {
+            favList.Add(fav);
+        }
+        public void AddExclude(string exc)
+        {
+            excludeList.Add(exc);
+        }
+        //remove
+        public void RemovePath(string path)
+        {
+            pathList.Remove(path);
+        }
+        public void RemoveFav(string fav)
+        {
+            favList.Remove(fav);
+        }
+        public void RemoveExclude(string exc)
+        {
+            excludeList.Remove(exc);
+        }
+
+        //get element
+        public string[] GetAllPaths()
+        {   
+            return pathList.ToArray();
+        }
+        public string[] GetAllFavs()
+        {
+            return favList.ToArray();
+        }
+        public string[] GetAllExcludes()
+        {
+            return excludeList.ToArray();
+        }
+        public int GetTimer()
+        {
+            return appParams[0];
+        }
+        public int GetBufferSize()
+        {
+            return appParams[1];
+        }
+        public int GetWallpaperStyle()
+        {
+            return appParams[2];
+        }
+        public int GetFavFreq()
+        {
+            return appParams[3];
+        }
+
+        //set
+        public void SetTimer(int data)
+        {
+            appParams[0] = data;
+        }
+        public void SetBufferSize(int data)
+        {
+            appParams[1] = data;
+        }
+        public void SetWallpaperStyle(int data)
+        {
+            appParams[2] = data;
+        }
+        public void SetFavFreq(int data)
+        {
+            appParams[3] = data;
+        }
+    }
+}
---
+++ BackgroundChanger/BackgroundChanger/Source/XMLHandler.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+/* Name: XmlHandler 
+ * Version: 00.00.01
+ */
+
+namespace BackgroundChanger.Source
+{
+    class XMLHandler
+    {
+        private string fileName;
+        private string rootNodeName;
+        private IList<string> userDatas;
+        private XmlDocument xml;
+
+        public XMLHandler()
+        {
+            fileName = "user.xml";
+            rootNodeName = "User";
+            userDatas = new List<string>();
+        }
+
+        private void CreateBase()
+        {
+            xml = new XmlDocument();
+            XmlNode rootNode = xml.CreateElement(rootNodeName);
+            xml.AppendChild(rootNode);
+
+            XmlNode rootDataNode;
+            XmlNode dataNode = xml.CreateElement("Path");
+
+            foreach (string userData in userDatas)
+            {
+                rootDataNode = xml.CreateElement(userData);
+                rootNode.AppendChild(rootDataNode);
+
+            }
+        }
+
+        /* 
+         * Create XML File
+         */
+        public void Create()
+        {
+            CreateBase();
+        }
+
+        public void Create(string file)
+        {
+            fileName = file;
+            CreateBase();
+        }
+
+        /* 
+         * Load XML File
+         */
+        public void Load()
+        {
+            xml = new XmlDocument();
+            xml.Load(fileName);
+        }
+
+        public void Load(string file)
+        {
+            xml = new XmlDocument();
+            fileName = file;
+            xml.Load(fileName);
+        }
+
+        /* 
+         * Save XML File
+         */
+        public void Save()
+        {
+            xml.Save(fileName);
+        }
+
+        /* Node: root Node 
+         * example: AddToRoot("Timer")
+         */
+        public void AddToRoot(string Node)
+        {
+            XmlNode rootDataNode = xml.DocumentElement;
+            XmlNode dataNode = xml.CreateElement(Node);
+            rootDataNode.AppendChild(dataNode);
+        }
+
+        /* Node: root Node 
+         * data: innerText data
+         * example: AddToRoot("Timer", "42")
+         */
+        public void AddToRoot(string Node, string data)
+        {
+            XmlNode rootDataNode = xml.DocumentElement;
+            XmlNode dataNode = xml.CreateElement(Node);
+            dataNode.InnerText = data;
+            rootDataNode.AppendChild(dataNode);
+        }
+
+        /* rootPath: root Node 
+         * Node: node name
+         * data: innerText data
+         * example: Add("//Nodes", "Node", "text")
+         */
+        public void Add(string rootPath, string Node, string data)
+        {
+            XmlNode dataNode = xml.CreateElement(Node);
+            dataNode.InnerText = data;
+            xml.DocumentElement.SelectSingleNode(rootPath).AppendChild(dataNode);
+        }
+
+        /* rootPath: root Node 
+         * strOut: text
+         * example: GetXMLNodes("//Nodes")
+         */
+        public string GetXMLNode(string rootPath)
+        {
+            if (File.Exists(fileName))
+            {
+                XmlNode rootNode = xml.DocumentElement.SelectSingleNode(rootPath);
+                return rootNode.InnerText;
+            }
+            else
+            {
+                return "-1";
+            }
+        }
+
+        /* rootPath: root Node 
+         * listOut: string list
+         * example: GetXMLNodes("//Nodes", ref list)
+         */
+        public void GetXMLNodes(string rootPath, ref IList<string> listOut)
+        {
+            XmlDocument xml = new XmlDocument();
+            xml.Load(fileName);
+            listOut.Clear();
+            XmlNode rootNode = xml.DocumentElement.SelectSingleNode(rootPath);
+            foreach (XmlNode childNode in rootNode)
+            {
+                listOut.Add(childNode.InnerXml);
+            }
+        }
+
+        /* rootPath: root Node 
+         * text: string list
+         * example: RemoveNodeByInnerText("//Nodes", "data1")
+         */
+        public void RemoveNodeByInnerText(string rootPath, string text)
+        {
+            XmlNode rootNode = xml.DocumentElement.SelectSingleNode(rootPath);
+            foreach (XmlNode childNode in rootNode)
+            {
+                if (childNode.InnerText.Equals(text))
+                {
+                    childNode.ParentNode.RemoveChild(childNode);
+                    break;
+                }
+            }
+        }
+
+        /* rootPath: root Node 
+          * text: string list
+          * example: ChangeNodeInnerText("//Timer", "50")
+          */
+        public void ChangeNodeInnerText(string rootPath, string text)
+        {
+            XmlNode rootNode = xml.DocumentElement.SelectSingleNode(rootPath);
+            rootNode.InnerText = text;
+        }
+    }
+}
